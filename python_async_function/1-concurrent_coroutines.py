4d58659f4b19bb18fd611e834265a183be25d58c
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 09:38:45 +0200

    Refactor wait_random me trompais de fichier :)

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-""" aaa """
+""" asynchronous routines in Python """
+
 import asyncio
 import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """ asynchronous routine that takes in two arguments """
+    listeDeSchindler = []
+    for i in range(n):
+        listeDeSchindler.append(await wait_random(max_delay))
+    return sorted(listeDeSchindler)
