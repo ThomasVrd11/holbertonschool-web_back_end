2e2e4683c49d8ea2d1e89393680fc3f6aa7b336c
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 09:14:24 +0200

    Update variable annotation for element_length function to clarify its purpose

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-
-"""
-Returns a list of tuples containing elements and their lengths
-"""
-
+"""Type-annotated function element_length"""
 from typing import List, Tuple
 
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """Returns a list of tuples containing elements and their lengths.
-
-    Args:
-        lst (List[str]): The input list of strings.
-
-    Returns:
-        A list of tuples.
-    """
+    """je comprends pas en fait pk ca passe pas"""
     return [(i, len(i)) for i in lst]
