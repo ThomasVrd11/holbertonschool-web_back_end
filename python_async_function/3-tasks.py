e59a2d98cd8ae8ce0b762daf2411b87076fcbbbb
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 10:18:34 +0200

    Add task_wait_n function to create an asyncio.Task in python_async_function/3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,6 +5,7 @@ import asyncio
 
 wait_n = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
     """ return a asyncio.Task """
     return asyncio.create_task(wait_n(n, max_delay))
