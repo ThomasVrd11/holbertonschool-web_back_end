a423ed4502218a38756b74973ad7a3808a20cadb
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 11:08:43 +0200

    Refactor async_generator function to use i instead of Audrey in python_async_comprehension/0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import random
 import asyncio
-
+from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """Coroutine"""
-    for Audrey in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
