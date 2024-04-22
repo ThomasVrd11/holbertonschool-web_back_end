daf49afdeea669e440ff816b78effce0bf7e65a8
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 09:35:35 +0200

    Refactor wait_random function to use wait_n in python_async_function/0-basic_async_syntax.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,9 +4,10 @@
 import asyncio
 import random
 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_random(max_delay: int = 10) -> float:
-    """ ça me fait chier de commenter """
-    NicolasTaillepierre = random.uniform(0, max_delay)
-    await asyncio.sleep(NicolasTaillepierre)
-    return NicolasTaillepierre
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """ asynchronous routine that takes in two arguments """
+    ChongGoat = [wait_random(max_delay) for i in range(n)]
+    return [await delay for delay in asyncio.as_completed(ChongGoat)]
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+""" aaa """
+import asyncio
+import random
