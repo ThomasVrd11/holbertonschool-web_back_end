a7ebb7b72e32d9fde2017689fa063c8f358ba6df
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 09:21:54 +0200

    je vais faire exploser l'ecole

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Type-annotated function element_length"""
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """je comprends pas en fait pk ca passe pas"""
     return [(i, len(i)) for i in lst]
