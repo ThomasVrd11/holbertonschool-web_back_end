e24f585b5fa14b118a79af27c19b049df602c4fe
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 10:35:46 +0200

    Refactor task_wait_n function to task_wait_random in python_async_function/3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -3,9 +3,10 @@
 
 import asyncio
 
-wait_n = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
-    """ return a asyncio.Task """
-    return asyncio.create_task(wait_n(n, max_delay))
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """return asyncio.Task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
