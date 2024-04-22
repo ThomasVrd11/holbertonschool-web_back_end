1b62c92a9610316c0dab0a433af3dfa62a71d363
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-22 09:27:01 +0200

    Add basic async syntax example and README.md file to python_async_function directory

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" asynchronous routines in Python """
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ ça me fait chier de commenter """
+    NicolasTaillepierre = random.uniform(0, max_delay)
+    await asyncio.sleep(NicolasTaillepierre)
+    return NicolasTaillepierre
