b3f5272dfb1274bb7864c9e3cbbf850f195f2bd1
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-29 09:39:44 +0200

    Add pagination helper function for zero-based indexing

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" zerooooooooo"""
+from typing import Tuple
+
+def index_range(page, page_size):
+    """ zerooooooooo"""
+    end = page * page_size
+    start = end - page_size
+    return (start, end)
