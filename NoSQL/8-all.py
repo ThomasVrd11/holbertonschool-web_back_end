3ddb870d1f53d16f6e2a58535aeb4fb996b2d1b0
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-29 09:05:03 +0200

    Add NoSQL database query script

---
+++ NoSQL/8-all.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Christophe a du aller chez le docteur parce que c'est un trop gros baiseur (2009)"""
+
+def list_all(mongo_collection):
+    """ o_o """
+    return list(mongo_collection.find())
