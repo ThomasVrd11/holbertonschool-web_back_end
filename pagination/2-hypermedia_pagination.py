d277d97396950357c8ca529845574755f88bd122
ThomasVrd11 <Thomasviard@orange.fr>
2024-05-03 16:43:02 +0200

    comit

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -2,7 +2,7 @@
 """class to handle simple pagination"""
 import csv
 import math
-from typing import Tuple, List
+from typing import Tuple, List, Dict, Optional
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -44,3 +44,29 @@ class Server:
             return []
         else:
             return self.__dataset[indexRange[0]:indexRange[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10
+                  ) -> Dict[str, Optional[int]]:
+        """
+        returns a dictionary containing the following key-value pairs:
+            page_size: the length of the returned dataset page
+            page: the current page number
+            data: the dataset page (equivalent to return from previous task)
+            next_page: number of the next page, None if no next page
+            prev_page: number of the previous page, None if no previous page
+            total_pages: the total number of pages in the dataset as an integer
+        """
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
